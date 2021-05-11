8111ec9dcf0c6dfaa981d08f5816e8cb4cd5a405
Jordy Henry Pereira <jordy.hpereira@gmail.com>
2021-05-11 18:30:58 -0300

    Added Simple Material to Tiles
    - Added material to easilly differentiate tiles in game view
    - Added code to apply alternate materials to each tile

--- Assets/LegendOfSidia/SquaredTilesBoard.cs
+++ Assets/LegendOfSidia/SquaredTilesBoard.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 namespace LegendOfSidia {
     public class SquaredTilesBoard : Board
     {
+        public Material whiteMaterial;
+        public Material blackMaterial;
         public override void CreateBoard()
         {
+            bool useWhiteMaterial = false;
             for (int i = 0; i < rows; i++)
             {
+                useWhiteMaterial = !useWhiteMaterial;
                 for (int j = 0; j < columns; j++)
                 {
                     Vector3 tilePosition = new Vector3(i, transform.position.y, j);
-                    Instantiate(tilePrefab, tilePosition, Quaternion.identity, transform);
+                    GameObject tileGO = Instantiate(tilePrefab, tilePosition, Quaternion.identity, transform);
+                    tileGO.GetComponent<Renderer>().material = (useWhiteMaterial) ? whiteMaterial : blackMaterial;
+                    useWhiteMaterial = !useWhiteMaterial;
                 }
             }
         }
